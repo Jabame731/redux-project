daa47222b8f6e897626761ed9e21beaf1e4a6ce9
Jabame <justinetaglucop@gmail.com>
2022-12-20 22:53:22 +0800

    Fix Route Error

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,17 +1,10 @@
 import express from 'express';
 
-import {
-  login,
-  register,
-  getUserDetails,
-} from '../controllers/authController.js';
-import token from '../middlewares/authorizationMiddleware.js';
+import { login, register } from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/register', register);
 router.post('/login', login);
-
-router.get('/me', token, getUserDetails);
+router.post('/register', register);
 
 export default router;
